f8aeb24115045d99dc0cae1f6be451f6ac59242c
danidi-1 <112910252+danidi-1@users.noreply.github.com>
2022-09-28 17:29:04 -0500

    Add files via upload

---
+++ 4-dom.js
@@ -0,0 +1,7 @@
+let name = document.queryselector("#name");
+let name = document.queryselector("#age");
+let likesMusic = document.querySelector("#likesMusic");
+
+const link = document.querySelector("a");
+ link.textContent = "Mozilla Developer Network";
+ link.href = "hrrps://developer.mozilla.org";
